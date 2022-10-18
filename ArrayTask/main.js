bf5514ff736b9264013043f222acec31925a1b24
justzbodya <bogdan.paly1@gmail.com>
2022-10-18 18:17:40 +0300

    Update main.js

--- ArrayTask/main.js
+++ ArrayTask/main.js
@@ -17,38 +17,37 @@
 // const names = ['алексей', 'богдан', 'надя', 'геалекс', 'лаврентий', 'федя', 'сеня']
 // console.log(sortNames(names, 'desc'))
 
-const sortBySumOfDigit= (array)=>{
-    return array.sort((digit_a, digit_b)=>{
-
-        let sum_a = 0,
-            sum_b = 0;
-
-        for (const digit of String(digit_a)){
-            sum_a += +digit
-        }
-        console.log(sum_a)
-        for (const digit of String(digit_b)){
-            sum_b += +digit
-        }
-        console.log(sum_b)
-        return (sum_a > sum_b) ? (sum_a == sum_b)?  0 :  1 : -1 // if a < b => return -1, otherwise check if a != b => return 1(false), or 0(true)
-    })
-}
-
-const random_numbers = [13, 467, 2, 9, 1, 4321]
-console.log(sortBySumOfDigit(random_numbers))   // [1, 2, 13, 4321, 467]
-
-
-// const shuffle = (array)=>{
-//     for (let i = array.length - 1; i > 0; i--) {
-//         let j = Math.floor(Math.random() * (i + 1));
-//         [array[i], array[j]] = [array[j], array[i]];
-//     }
+// const sortBySumOfDigit= (array)=>{
+//     return array.sort((digit_a, digit_b)=>{
+//
+//         let sum_a = 0,
+//             sum_b = 0;
+//
+//         for (const digit of String(digit_a)){
+//             sum_a += +digit
+//         }
+//         console.log(sum_a)
+//         for (const digit of String(digit_b)){
+//             sum_b += +digit
+//         }
+//         console.log(sum_b)
+//         return (sum_a > sum_b) ? (sum_a == sum_b)?  0 :  1 : -1 // if a < b => return -1, otherwise check if a != b => return 1(false), or 0(true)
+//     })
 // }
 //
-// let random_numbers = [13, 467, 2, 9, 1, 4321]
-// shuffle(random_numbers)
-// console.log(random_numbers)
+// const random_numbers = [13, 467, 2, 9, 1, 4321]
+// console.log(sortBySumOfDigit(random_numbers))   // [1, 2, 13, 4321, 467]
+
+
+const shuffle = (array)=>{
+    array.sort(function(a, b) {
+        return Math.random() - 0.5
+    });
+}
+
+let random_numbers = [13, 467, 2, 9, 1, 4321]
+shuffle(random_numbers)
+console.log(random_numbers)
 
 // const STATUSES = {
 //     ONLINE: 'online',
